dcf16ed680ff122cae82ad4227a5cf278f80802e
ypftest <1099476631@qq.com>
2022-09-18 10:16:06 +0800

    刚开始

---
+++ src/main/java/com/example/springsecuritys/SpringSecuritysApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecuritys/SpringSecuritysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
